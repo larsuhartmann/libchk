c264dcb10e7d878a880b3d388bc3e8c285027b3a
Lars Hartmann <lars@chaotika.org>
2010-09-27 09:49:44 +0200

    finished main function.

--- 20_main.sh
+++ 20_main.sh
@@ -2,5 +2,17 @@ compat_libs=`get_compat_libs`
 
 for broke in `get_broken_libs`
 do
-    echo "broke: $broke"
+    pkg=`get_pkg $broke`
+    broken_pkgs="$broken_pkgs $pkg"
 done
+
+if [ "$broken_pkgs" ]
+then
+    bpkgs=`list2lines "$broken_pkgs"|auniq`
+    bpkgs=`lines2list "$bpkgs"`
+    echo "broken packages: $bpkgs"
+    echo "to fix them, run:"
+    echo "portmaster $bpkgs"
+else
+    echo "your system's linkage is healthy!"
+fi
